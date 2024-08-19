f0db2715b2f1c166d70fe77a639ea70e0b03f86c
flappyp <samdawson586@gmail.com>
2024-08-19 11:15:05 +0100

    Lab 4.1
    Adding player jump and spawning random objects

---
+++ SD_GD3_Week4/Assets/SpawnManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] obstaclePrefab; // allows me to add gameobjects in the inspector window
+    private Vector3 spawnPos = new Vector3(25,0,0); // declaring where the objects will spawn in the game
+    private float startDelay = 2; //how long before the first object spawns
+    private float repeatRate = 2; // the time between objects spawing in again
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       InvokeRepeating("SpawnObstacle", startDelay, repeatRate); //repeating the spawnobstacle function along with te start and repeat delays
+    }
+
+    void SpawnObstacle ()
+    {
+        int obstacleIndex = Random.Range(0, obstaclePrefab.Length); //getting the obstacleIndex and randomizing what obejct is being spawned 
+        Instantiate(obstaclePrefab[obstacleIndex], spawnPos, obstaclePrefab[obstacleIndex].transform.rotation);
+
+    }
+}
